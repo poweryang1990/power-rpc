0e805e87439f6cb1b00978bdbfeece2b947a4a9f
Power Yang <poweryang1990@hotmail.com>
2019-07-18 11:32:37 +0800

    ~

---
+++ sample-client/src/main/java/sample/rpc/client/SampleRpcClientApplication.java
@@ -0,0 +1,19 @@
+package sample.rpc.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import power.rpc.client.annotation.EnableRpcClients;
+
+/**
+ * @Author: PowerYang
+ * @Date: 2019/7/10 15:11
+ */
+@SpringBootApplication
+@ComponentScan({"**.rpc.**","rpc.**"})
+@EnableRpcClients({"rpc.sample.api"})
+public class SampleRpcClientApplication {
+    public static void main(String[] args)  {
+        SpringApplication.run(SampleRpcClientApplication.class, args);
+    }
+}
